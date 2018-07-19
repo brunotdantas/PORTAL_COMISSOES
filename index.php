509b406abc3095b369ef588ae81ae822890f249d
Bruno Dantas <bdantas@br.estee.com>
2018-07-19 19:33:54 -0300

    First_commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php //TODO se o usário logado acessar o index, direcionar pra landpage
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Portal de comissões</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/square/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>Portal de comissões</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Faça o seu login para começar a sessão</p>
+
+    <form action="src/valForms/validaLogin.php" method="POST">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" placeholder="Usuario" name="usuario">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Senha" name="senha">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+<!--              <input type="checkbox"> Remember Me -->
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat" name="botao" value="entrar">Entrar</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+<!--
+    <div class="social-auth-links text-center">
+      <p>- OR -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+        Google+</a>
+    </div>
+     /.social-auth-links -->
+
+    <a href="#" data-toggle="modal" data-target="#modal-enviar" >Esqueci minha senha</a><br>
+    <!--MODAL SOLICITANDO O E-MAIL CADASTRADO -->
+    <form action="src/valForms/valida_email.php" method="POST">
+    <div class="modal modal-info fade" id="modal-enviar">
+      <div class="modal-dialog"> <!--janela fica pequena e centralizada -->
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Solicitação para renovar senha</h4>
+          </div>
+          <div class="modal-body" >
+            <input type="email" class="form-control" name="email_cadastrado" id="inputEmail3" placeholder="Email"/>
+            <p>Informe o e-mail completo cadastrado no sistema</p>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" name="botao" value="enviar" class="btn btn-outline">Enviar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+    <!--<a href="register.html" class="text-center">Criar um novo usuário</a>-->
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="plugins/iCheck/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
